134e621b64edd6dcbcdefea73fbb7cbef70920ef
James Fletcher <james@grakn.ai>
2019-01-07 15:40:33 +0000

    More tests for the chainable

--- kgcn2/model/chainables.py
+++ kgcn2/model/chainables.py
@@ -4,7 +4,12 @@ import tensorflow as tf
 class DenseChainable:
 
     def __init__(self, num_outputs, dense_layer=tf.layers.Dense):
-        self._dense_layer = dense_layer(num_outputs)
+        self._num_outputs = num_outputs
+        self._dense_layer = dense_layer(self._num_outputs)
 
     def __call__(self, input_tensor):
+        input_tensor_size = input_tensor.shape.as_list()
+        if input_tensor_size[1] != self._num_outputs:
+            raise ValueError(f'Input tensor has dimension {input_tensor_size[1]}, expected {self._num_outputs}')
+
         return self._dense_layer(input_tensor)
--- kgcn2/model/test/chainables_test.py
+++ kgcn2/model/test/chainables_test.py
@@ -9,8 +9,8 @@ Components should interface to one another so that they can be chained. They sho
 and have the capacity for a full count input for the case that they are the terminating end of the query.
 
 Requirements:
--   Takes the same number of inputs as it gives outputs
--   The same component is reused everywhere that the type is used
+-   Takes the same number of inputs as it gives outputs (should throw an error if untrue)
+-   The same component (or it's characteristics e.g. weights, offsets) is reused everywhere that the type is used
 -   (Possibly) Components should accept an input to indicate the proportion of results found that were unique for this
     type
 """
@@ -37,12 +37,65 @@ class TestDenseChainable(unittest.TestCase):
         # Create a session and do any necessary initialisation
         tf_session = tf.Session()
         init_global = tf.global_variables_initializer()
-        init_local = tf.local_variables_initializer()  # Added to initialise tf.metrics.recall
-        init_tables = tf.tables_initializer()
 
         tf_session.run(init_global)
-        tf_session.run(init_local)
-        tf_session.run(init_tables)
 
         output_vector = tf_session.run(output_tensor, {input_placeholder: input_vector})
         self.assertEqual(expected_output_vector_shape, output_vector.shape)
+
+    def test_if_input_shape_wrong_raise_exception(self):
+        chainable = chainables.DenseChainable(7)
+
+        input_placeholder = tf.placeholder(tf.float32, shape=(20, 5))
+        with self.assertRaises(ValueError) as context:
+            chainable(input_placeholder)
+
+        self.assertTrue('Input tensor has dimension 5, expected 7' in str(context.exception))
+
+    def test_component_reuse(self):
+        input_vector = np.ones((20, 5))
+
+        def _create_dense(units):
+            return tf.layers.Dense(units, activation=tf.nn.relu, kernel_initializer=tf.random_uniform_initializer)
+
+        chainable = chainables.DenseChainable(5, dense_layer=_create_dense)
+
+        input_placeholder_1 = tf.placeholder(tf.float32, shape=(20, 5))
+        output_tensor_1 = chainable(input_placeholder_1)
+
+        input_placeholder_2 = tf.placeholder(tf.float32, shape=(20, 5))
+        output_tensor_2 = chainable(input_placeholder_2)
+
+        # Create a session and do any necessary initialisation
+        tf_session = tf.Session()
+        init_global = tf.global_variables_initializer()
+
+        tf_session.run(init_global)
+
+        output_vector_1, output_vector_2 = tf_session.run((output_tensor_1, output_tensor_2),
+                                                          {input_placeholder_1: input_vector,
+                                                           input_placeholder_2: input_vector})
+        # output_vector_1 = tf_session.run(output_tensor_1, {input_placeholder_1: input_vector})
+        # output_vector_2 = tf_session.run(output_tensor_2, {input_placeholder_2: input_vector})
+        np.testing.assert_array_equal(output_vector_1, output_vector_2)
+
+    def test_component_chains_with_itself(self):
+        input_vector = np.ones((20, 5))
+
+        def _create_dense(units):
+            return tf.layers.Dense(units, activation=tf.nn.relu, kernel_initializer=tf.random_uniform_initializer)
+
+        chainable = chainables.DenseChainable(5, dense_layer=_create_dense)
+
+        input_placeholder = tf.placeholder(tf.float32, shape=(20, 5))
+        output_tensor_1 = chainable(input_placeholder)
+
+        output_tensor_2 = chainable(output_tensor_1)
+
+        # Create a session and do any necessary initialisation
+        tf_session = tf.Session()
+        init_global = tf.global_variables_initializer()
+
+        tf_session.run(init_global)
+
+        output_vector_2 = tf_session.run(output_tensor_2, {input_placeholder: input_vector})
